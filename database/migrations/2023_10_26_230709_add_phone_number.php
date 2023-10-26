6476c845d02e961fd7e36134fb3086ad9696d578
Rahul Suthar <rahul@unicloudtech.com>
2023-10-27 04:57:50 +0530

    addcolumn

---
+++ database/migrations/2023_10_26_230709_add_phone_number.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPhoneNumber extends Migration {
+    public function up() {
+        Schema::table('student', function (Blueprint $table) {
+            $table->string('phone_number')->nullable();
+        });
+    }
+
+    public function down() {
+        Schema::table('student', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+        });
+    }
+}
