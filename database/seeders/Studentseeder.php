02705d2626d703fc8159c2a0f146ffa4bd7fba9d
Rahul Suthar <rahul@unicloudtech.com>
2023-10-27 10:09:19 +0530

    databse seeding

--- database/seeders/Studentseeder.php
+++ database/seeders/Studentseeder.php
@@ -4,14 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Student;
 
 class Studentseeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Student::create([
+            'name'=>'Rahul raghvendra',
+            'email'=> 'rs123@gmail.com',
+            'class'=>'8',
+            'section'=>'A',
+            'gender'=>'male']);
     }
 }
