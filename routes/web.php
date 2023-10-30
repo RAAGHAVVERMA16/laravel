306a533db4a38b16768c2254525749a436bbdc6b
Raaghav <raaghav@unicloudtech.com>
2023-10-30 13:12:18 +0530

    / redirection one restored

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-// return view('welcome');
-// });
+Route::get('/', function () {
+return view('welcome');
+});
 use \App\Http\Controllers\FormController;
 use \App\Http\Controllers\StudentController;
 use \App\Http\Middleware\Authenticate;
 use \App\Http\Controllers\LoginController;
 use \App\Http\Controllers\Dashboardcontroller;
-/*
-Route::get('/users/{id?}', function ($id) {
-    return 'this is user ' . $id;
-});
-Route::get('/counter', '\App\Http\Controllers\Counter@index');
 
-Route::get('/', '\App\Http\Controllers\HomeController@index');
-Route::get('/about', '\App\Http\Controllers\AboutController@index');
-Route::get('/contact', '\App\Http\Controllers\ContactController@index');
-Route::get('/form', '\App\Http\Controllers\formController@index');
-*///temporaririly blocked
+// Route::get('/users/{id?}', function ($id) {
+//     return 'this is user ' . $id;
+// });
+// Route::get('/counter', '\App\Http\Controllers\Counter@index');
+
+//Route::get('/', '\App\Http\Controllers\HomeController@index');
+// Route::get('/about', '\App\Http\Controllers\AboutController@index');
+// Route::get('/contact', '\App\Http\Controllers\ContactController@index');
+// Route::get('/form', '\App\Http\Controllers\formController@index');
+//temporaririly blocked
 
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
