463369a1249ab1b6752fe498d146b0762c4f2e24
Rahul Suthar <rahul@unicloudtech.com>
2023-10-31 11:21:36 +0530

    sir gave file for auto clearing .

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,14 @@ Route::get('delete/{id}', [StudentController::class, 'destroy'])->name('delete')
 Route::group(['middleware'=> ['auth:admin']],function(){
     Route::get('/users', [UserController::class, 'users']);
 });
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('route:clear');
+    $exitCode = Artisan::call('view:clear');
+    die("clear-cache");
+    // return what you want
+});
 
 
 
